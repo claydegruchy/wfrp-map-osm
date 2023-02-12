398e3a756b6d4662b34b3e5ff60158e3dbfe1d69
clay <clay.degruchy+github@gmail.com>
2023-02-12 12:06:27 +0100

    added maps

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import './style.css';
 import { Map, View } from 'ol';
-import { defaults as defaultControls } from 'ol/control.js';
-
-import ImageLayer from 'ol/layer/Image.js';
-import Projection from 'ol/proj/Projection.js';
-import Static from 'ol/source/ImageStatic.js';
+import {OverviewMap,defaults as defaultControls } from 'ol/control.js';
 import { getCenter } from 'ol/extent.js';
 import XYZ from 'ol/source/XYZ.js';
 import { createStringXY } from 'ol/coordinate.js';
-import { boundingExtent } from 'ol/extent.js';
+import TileGrid from 'ol/tilegrid/TileGrid.js';
+import { Group, Tile } from 'ol/layer.js';
+import TileImage from 'ol/source/TileImage.js';
 
-import { transformExtent, transform } from 'ol/proj';
+
+
+import { transform } from 'ol/proj';
 
 //handle scrolling
 import { DragPan, MouseWheelZoom, defaults } from 'ol/interaction.js';
@@ -30,76 +30,94 @@ const mousePositionControl = new MousePosition({
   target: document.getElementById('mouse-position'),
 });
 
-// image placer
 
 
+const world = new XYZ({
+  // url:  'http://www.gitzmansgallery.com/tiles/{z}_{x}_{y}.jpg',
+  tileUrlFunction: ([z, x, y]) => {
+    if (z > 7) return null
+    return `http://www.gitzmansgallery.com/tiles/${z}_${x}_${y}.jpg`
+  }
+})
 
-// new ImageLayer({
-//   source: new Static({
-//     attributions: '© <a href="https://xkcd.com/license.html">xkcd</a>',
-//     url: 'https://imgs.xkcd.com/comics/online_communities.png',
-//     // url: '/marienberg.tiff',
-
-//     projection: projection,
-//     imageExtent: extent,
-//   }),
-// }),
-// new TileLayer({
-//   source: new OSM()
-// }), 
-
-var mercatorExtent = [0, 0, 300000, 300000];
-
-
-var x = transformExtent(mercatorExtent, 'EPSG:3857', 'EPSG:4326');
-// console.log(transform([-16, 32], 'EPSG:4326', 'EPSG:3857'));
-transform([-16, 32], 'EPSG:4326', 'EPSG:3857')
-
-
-// aldorf size
-// 3366 × 3248
-
+const overviewMapControl = new OverviewMap({
+  collapsed:false,
+  // className:'custom-overview',
+  layers: [
+    new TileLayer({
+      source: world,
+    }),
+  ],
+});
 
 
 const map = new Map({
   target: 'map',
-  controls: defaultControls().extend([mousePositionControl]),
+  controls: defaultControls().extend([mousePositionControl,overviewMapControl]),
 
   layers: [
     // main map
     new TileLayer({
-      maxZoom: 7,
-      source: new XYZ({
-        url:
-          'http://www.gitzmansgallery.com/tiles/{z}_{x}_{y}.jpg'
-      }),
+      // maxZoom: 7,
+      source: world
+
     }),
-    // city
-    new ImageLayer({
-      // minZoom: 5,
 
 
+    new Group({
+      title: 'Altdorf Overlay',
+      layers: [
+        new Tile({
+          title: 'Altdorf Tile',
+          minZoom: 7,
 
-      source: new Static({
-        url:
-          '/Map-City-Altdorf-4-Color.jpg',
-        crossOrigin: '',
-        projection: 'EPSG:3857',
+          // opacity: 0.7,
+          source: new TileImage({
+            attributions: '',
+            tileGrid: new TileGrid({
+              extent: [-1879572.67834710842, 3734469.05619834783, -1754782.27834710851, 3824113.85619834764],
+              origin: [-1879572.67834710842, 3734469.05619834783],
+              resolutions: [537.600000000000023, 268.800000000000011, 134.400000000000006, 67.2000000000000028, 33.6000000000000014, 16.8000000000000007, 8.40000000000000036, 4.20000000000000018],
+              tileSize: [256, 256]
+            }),
+            tileUrlFunction: function (tileCoord) {
+              return ('./altdorf/{z}/{x}/{y}.png'
+                .replace('{z}', String(tileCoord[0]))
+                .replace('{x}', String(tileCoord[1]))
+                .replace('{y}', String(- 1 - tileCoord[2])));
+            },
+          })
+        }),
+      ]
+    }),
 
-        // imageExtent: boundingExtent([transform([0, 49], "EPSG:4326", "EPSG:32661"),
-        // transform([10, 55], "EPSG:4326", "EPSG:32661")]),
 
-        // imageExtent: [-1852995.9934683219, 3839394.002326335,-1754102.8280948512, 3755827.8914180356],
-        imageExtent: [0, 0, 300000, 300000],
-
-        // imageExtent: [-2397440.0539429057, 4246479.048244938, -926984.7401089855, 3146059.53083352],
-
-        interpolate: false,
-        opacity: 0.5
-      })
-    })
+    new Group({
+      title: 'Marienburg Overlay',
+      layers: [
+        new Tile({
+          title: 'Marienburg Tile',
+          minZoom: 7,
+          // opacity: 0.7,
+          source: new TileImage({
+            attributions: '',
+            tileGrid: new TileGrid({
+              extent: [-3320017.90883266041, 4701281.93297282793, -3175634.72078718198, 4803354.42623188905],
+              origin: [-3320017.90883266041, 4701281.93297282793],
+              resolutions: [658.532214574588806, 329.266107287294403, 164.633053643647202, 82.3165268218236008, 41.1582634109118004, 20.5791317054559002, 10.2895658527279501, 5.14478292636397505],
+              tileSize: [256, 256]
+            }),
+            tileUrlFunction: function (tileCoord) {
+              return ('./marienburg/{z}/{x}/{y}.png'
+                .replace('{z}', String(tileCoord[0]))
+                .replace('{x}', String(tileCoord[1]))
+                .replace('{y}', String(- 1 - tileCoord[2])));
+            },
+          })
+        }),
+      ]
+    }),
   ],
-
 
 
 
@@ -113,5 +131,6 @@ const map = new Map({
 
 });
 map.on('singleclick', function (event) {
-  console.log(event.coordinate);
+  console.log(event.coordinate, transform(event.coordinate, 'EPSG:3857', 'EPSG:4326'));
+  // transform(event.coordinate, 'EPSG:3857', 'EPSG:4326');
 })
