103a88fa883e6f8ff23fc9c375700561c16d737c
Clay DeGruchy <clay.degruchy+github@gmail.com>
2024-01-11 14:43:49 +0100

    this fucking sucks ass, wish i'd never started. dont know what i changed

--- src/components/Scenes.svelte
+++ src/components/Scenes.svelte
@@ -8,49 +8,22 @@
   import VectorLayer from "./VectorLayer.svelte";
   import { sceneStyleGenerator } from "$lib/olStyleBuilder.js";
 
+  import { Interaction } from "ol/interaction.js";
+  import { click, pointerMove, altKeyOnly } from "ol/events/condition";
+
   const pointsRef = get(store_pointsRef);
   console.log("[Scenes] initated");
 
   const mapInstance = getMapInstance();
 
-  let info;
-  let currentFeature;
-
-  const displayFeatureInfo = (pixel) => {
-    const feature = featureAtPixel(pixel, mapInstance);
-    if (!feature) return;
-    console.log(feature?.values_.name);
-    if (feature) {
-      info.style.left = pixel[0] + "px";
-      info.style.top = pixel[1] + "px";
-      if (feature !== currentFeature) {
-        info.style.visibility = "visible";
-        info.innerText = feature.get("name");
-      }
-    } else {
-      info.style.visibility = "hidden";
-    }
-    currentFeature = feature;
-  };
-
-  mapInstance.on("pointermove", function (evt) {
-    if (evt.dragging) {
-      info.style.visibility = "hidden";
-      currentFeature = undefined;
-      return;
-    }
-    const pixel = mapInstance.getEventPixel(evt.originalEvent);
-    displayFeatureInfo(pixel, evt.originalEvent.target);
+  let mv = new Interaction({
+    condition: pointerMove,
+    onSelect: (e) => {
+      console.log(e);
+    },
   });
 
-  mapInstance.on("click", function (evt) {
-    displayFeatureInfo(evt.pixel, evt.originalEvent.target);
-  });
-
-  mapInstance.getTargetElement().addEventListener("pointerleave", function () {
-    currentFeature = undefined;
-    info.style.visibility = "hidden";
-  });
+  mapInstance.addInteraction(mv);
 
   const styleGenerator = (feature, resolution) =>
     sceneStyleGenerator(feature, resolution);
@@ -76,24 +49,3 @@
     {/each}
   </VectorLayer>
 </Collection>
-
-<div id="info" bind:this={info}></div>
-
-<style>
-  #info {
-    position: absolute;
-    display: inline-block;
-    height: auto;
-    width: auto;
-    z-index: 100;
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    border-radius: 4px;
-    padding: 5px;
-    left: 50%;
-    transform: translateX(3%);
-    visibility: hidden;
-    pointer-events: none;
-  }
-</style>
