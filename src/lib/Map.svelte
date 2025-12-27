fdeb95d4f168327f466096a0b0ee833abbbbd7c8
Clay DeGruchy <clay.degruchy+github@gmail.com>
2025-12-27 17:02:45 +0100

    added all maps

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -3,64 +3,42 @@
   import Map from "ol/Map.js";
   import View from "ol/View.js";
   import TileLayer from "ol/layer/Tile.js";
-  import OSM from "ol/source/OSM.js";
-  import { TileImage, XYZ } from "ol/source";
+  import { XYZ } from "ol/source";
   import { map } from "./stores";
   import "ol/ol.css";
   import { TileGrid } from "ol/tilegrid";
+  import {
+    Marienburg,
+    AltdorfMap,
+    BelAliad,
+    Carroburg,
+    Kemperbad,
+    Miragliano,
+    Nuln,
+    Praag,
+    Sartosa,
+    Ubersreik,
+    world,
+  } from "./maps";
 
   let center = [-3247495.2505356777, 4704319.403427397];
   let zoom = 5;
-
-  const imageStorageURL = "https://claydegruchy.github.io/wfrp-map-storage";
-  const mainMapUrl = imageStorageURL + "/world-map/{z}/{z}_{x}_{y}.jpg";
-
-  const marienburgTileGrid = new TileGrid({
-    extent: [
-      -3320017.90883266041, 4701281.93297282793, -3175634.72078718198,
-      4803354.42623188905,
-    ],
-    origin: [-3320017.90883266041, 4701281.93297282793],
-    resolutions: [
-      658.5322145745888, 329.2661072872944, 164.6330536436472, 82.3165268218236,
-      41.1582634109118, 20.5791317054559, 10.28956585272795, 5.144782926363975,
-    ],
-    tileSize: [256, 256],
-  });
 
   onMount(() => {
     $map = new Map({
       target: "map",
       layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: mainMapUrl,
-            maxZoom: 7,
-          }),
-        }),
-
-        new TileLayer({
-          preload: 3,
-          source: new XYZ({
-            tileGrid: marienburgTileGrid,
-            tileUrlFunction: (tileCoord) => {
-              const [z, x, y] = tileCoord;
-              let url =
-                imageStorageURL +
-                "/" +
-                "marienburg/" +
-                z +
-                "/" +
-                x +
-                "/" +
-                (-1 - y) +
-                ".png";
-              console.log(url);
-
-              return url;
-            },
-          }),
-        }),
+        world,
+        Marienburg,
+        AltdorfMap,
+        BelAliad,
+        Carroburg,
+        Kemperbad,
+        Miragliano,
+        Nuln,
+        Praag,
+        Sartosa,
+        Ubersreik,
       ],
       view: new View({ center, zoom }),
     });
