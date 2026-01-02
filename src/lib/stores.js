ea06e612557b529160dc06abe20c8e0dc07444e0
Clay DeGruchy <clay.degruchy+github@gmail.com>
2026-01-02 14:04:46 +0100

    add load and save basics

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,16 +1,22 @@
 import { writable } from "svelte/store";
 
+
+
+const urlParams = new URLSearchParams(window.location.search);
+export const isDev = urlParams.get("dev") === "true";
+export const isEditMode = urlParams.get("edit") && urlParams.get("edit") != "";
+export const editId = isEditMode && urlParams.get("edit")
+
+
+
 export const map = writable();
 export const selectedLocations = writable([])
 export const diagnosticFeatures = writable([]);
 
-export const localRoutes = writable([])
-export const localLocations = writable([])
-
-const urlParams = new URLSearchParams(window.location.search);
-export const isDev = urlParams.get("dev") === "true";
-export const isAddMode = urlParams.get("add") && urlParams.get("add") != "";
-export const addId = isAddMode && urlParams.get("add")
+const r = loadLocalStorage("routes")
+export const localRoutes = writable(r ? Object.values(r) : [])
+const l = loadLocalStorage("locations")
+export const localLocations = writable(l ? Object.values(l) : [])
 
 
 function saveLocalStorage(key, obj) {
@@ -20,22 +26,54 @@ function saveLocalStorage(key, obj) {
 }
 function loadLocalStorage(key) {
 	console.log("loadLocalStorage", key);
-	return JSON.parse(localStorage.getItem(key))
+
+	try {
+		const raw = localStorage.getItem(key)
+		if (raw === null) return null
+		console.log(raw);
+
+		return JSON.parse(raw)
+	} catch {
+		return null
+	}
 }
 
 
+export let localRoutesObjectNonStore = {}
 localRoutes.subscribe(routes => {
-	saveLocalStorage("routes", Object.fromEntries(routes.map(route => [route.source_id + ":" + route.destination_id, route])))
+	localRoutesObjectNonStore = Object.fromEntries(routes.map(route => [route.source_id + ":" + route.destination_id, route]))
+	saveLocalStorage("routes", localRoutesObjectNonStore)
 })
-
+export let localLocationsObjectNonStore = {}
 localLocations.subscribe(locations => {
-	saveLocalStorage("locations", Object.fromEntries(locations.map(location => [location.id, location])))
+	localLocationsObjectNonStore = Object.fromEntries(locations.map(location => [location.id, location]))
+	saveLocalStorage("locations", localLocationsObjectNonStore)
 })
 
 
-export const addLocationTags = writable("")
+export const addLocationTags = writable(loadLocalStorage("addLocationTags"))
+console.log(loadLocalStorage("addLocationTags"));
 addLocationTags.subscribe(
 	lut => {
 		saveLocalStorage("addLocationTags", lut)
 	}
 )
+
+
+export function clearLocalFeatures() {
+	localStorage.removeItem("routes")
+	localStorage.removeItem("locations")
+	window.location.reload();
+
+}
+
+
+export function clearAll() {
+	localStorage.clear()
+	window.location.reload();
+}
+
+
+function importFeatures(file) {
+
+}
